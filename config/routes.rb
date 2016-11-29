713f5ddb722b45cea563033aee15fcea4be2758b
nnickd <nicolas.dellitalia@gmail.com>
2016-11-29 11:13:00 -0500

    spiked out signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/show'
+  resource :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
