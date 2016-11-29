4de342052244cd1b2bc711a37120a466802007f4
nnickd <nicolas.dellitalia@gmail.com>
2016-11-29 11:14:55 -0500

    added name and email to users form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,15 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  def create
+    @user = User.new user_params
+    return render :new unless @user.save
+    redirect_to root_path, notice: 'Created user'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :name, :email, :password, :password_confirmation)
+  end
 end
