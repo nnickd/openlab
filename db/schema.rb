021952926937298879e0a9ac03e0c72c070d0600
nnickd <nicolas.dellitalia@gmail.com>
2016-11-30 20:54:41 -0500

    generated log model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130174742) do
+ActiveRecord::Schema.define(version: 20161201015353) do
+
+  create_table "logs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_logs_on_project_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.integer  "user_id"
