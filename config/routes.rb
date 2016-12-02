b55b96aebbb791f2976767214ee19b062e09598f
nnickd <nicolas.dellitalia@gmail.com>
2016-12-02 14:00:55 -0500

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'sessions#home'
-  resources :projects
+  
+  post '/payments' => 'payments#create'
+  post '/logs' => 'logs#create'
+
   resource :sessions
   resource :users
+  resources :projects
 
-  resources :logs
-  resources :payments
-
-  # post '/logs' to: 'logs#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
