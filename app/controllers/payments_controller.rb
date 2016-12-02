c102b51eee023aeadc8ef64cfcb0c51b0a26ccde
nnickd <nicolas.dellitalia@gmail.com>
2016-12-01 20:47:03 -0500

    changed charges to payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,4 @@
-class ChargesController < ApplicationController
+class PaymentsController < ApplicationController
 
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resource :users
 
   resources :logs
-  resources :charges
+  resources :payments
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
