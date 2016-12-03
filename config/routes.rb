27e6e70fc91df5fc9cdaf5f8421b301629bc63dc
nnickd <nicolas.dellitalia@gmail.com>
2016-12-03 17:58:02 -0500

    search button

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,4 @@ Rails.application.routes.draw do
   resource :sessions
   resource :users
 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
