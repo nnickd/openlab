01b886aeac63eb9a46a20a295678b40b2c9ea102
nnickd <nicolas.dellitalia@gmail.com>
2016-12-05 16:26:48 -0500

    routes and erd

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,12 @@ Rails.application.routes.draw do
 
   root 'sessions#home'
 
-  post '/logs' => 'logs#create'
-  patch '/logs' => 'logs#update'
-  put '/logs' => 'logs#update'
-  delete '/logs' => 'logs#destroy'
-
   post '/payments' => 'payments#create'
   post '/pools' => 'pools#create'
   post '/images' => 'images#create'
 
-  # post '/videos' => 'videos#create'
-  # patch '/videos' => 'videos#update'
-  # put '/videos' => 'videos#update'
-  # delete '/videos' => 'videos#destroy'
-
   resources :videos, except: [:index, :show]
+  resources :logs, except: [:index, :show]
 
   resources :projects
   resource :sessions
