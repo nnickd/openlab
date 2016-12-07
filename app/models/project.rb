c120eea7c4ff0e6797ff10d2a1d2fcddc2518429
nnickd <nicolas.dellitalia@gmail.com>
2016-12-07 11:41:00 -0500

    changing user log relationship

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_one :pool, dependent: :destroy
+  has_one :pool
   has_many :logs, dependent: :destroy
   has_many :images, dependent: :destroy
   has_one :video, dependent: :destroy
