d7967991a62c1676a94a6e44bdaa3a6167ec1eab
nnickd <nicolas.dellitalia@gmail.com>
2016-12-07 14:55:23 -0500

    generate category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207181924) do
+ActiveRecord::Schema.define(version: 20161207195418) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "science"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "contents", force: :cascade do |t|
     t.integer  "project_id"
