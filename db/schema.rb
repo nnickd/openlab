28b68e655451ff33a42f4505c1b1579b7e04755e
nnickd <nicolas.dellitalia@gmail.com>
2016-12-07 11:21:09 -0500

    migrate remove content columns from projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205190708) do
+ActiveRecord::Schema.define(version: 20161207161944) do
 
   create_table "images", force: :cascade do |t|
     t.datetime "created_at",       null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20161205190708) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "tag"
     t.index ["project_id"], name: "index_logs_on_project_id"
   end
 
@@ -55,12 +56,8 @@ ActiveRecord::Schema.define(version: 20161205190708) do
   create_table "projects", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "title"
-    t.text     "about"
-    t.text     "context"
-    t.text     "significance"
-    t.text     "goals"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean  "posted"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
