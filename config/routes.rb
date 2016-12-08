08a898aba332514cc5bdbb5b47f480d5138ff3ee
nnickd <nicolas.dellitalia@gmail.com>
2016-12-08 18:30:17 -0500

    fixed js bug

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   post '/payments' => 'payments#create'
   post '/pools' => 'pools#create'
 
-
-  resources :videos, except: [:show, :index]
   resource :sessions
   resource :users
 
+  resources :videos, except: [:show, :index]
   resources :projects, shallow: true do
     resources :categories_projects
     resources :images
