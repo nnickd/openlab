1d9bb480083beb7b6f28077ddceda96e565a1648
nnickd <nicolas.dellitalia@gmail.com>
2016-12-10 12:17:24 -0500

    associated relationships between user stripe_account and address

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   has_many :projects, dependent: :destroy
   has_many :payments
   has_many :logs
+  has_one :stripe_account
   validates :username, presence: true
 end
