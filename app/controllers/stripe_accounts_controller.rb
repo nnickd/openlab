0a039d166a0609b7f9c6a219bf277e0dc6c1e9b9
nnickd <nicolas.dellitalia@gmail.com>
2016-12-10 14:44:12 -0500

    can create managed account

--- app/controllers/stripe_accounts_controller.rb
+++ app/controllers/stripe_accounts_controller.rb
@@ -2,7 +2,20 @@ class StripeAccountsController < ApplicationController
   def create
     @stripe_account = StripeAccount.new(stripe_account_params)
     return redirect_to users_path unless @stripe_account.save
-    redirect_to users_path, notice: 'stripe_account was successfully posted.'
+
+    Stripe.api_key = Rails.configuration.stripe[:secret_key]
+    Stripe::Account.create(
+      managed: true,
+      country: 'US',
+      legal_entity: {
+        type: 'individual',
+        first_name: @stripe_account.first_name,
+        last_name: @stripe_account.last_name,
+        ssn_last_4: @stripe_account.last_4_social
+      }
+    )
+
+    redirect_to users_path, notice: 'stripe_account was successfully created.'
   end
 
   private
