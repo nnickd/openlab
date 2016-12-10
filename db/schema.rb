b994d19002acdf3798349ac4a986f7419a97ab4b
nnickd <nicolas.dellitalia@gmail.com>
2016-12-10 13:22:27 -0500

    migration add managed_id to stripe_account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210171236) do
+ActiveRecord::Schema.define(version: 20161210182112) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "city"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 20161210171236) do
     t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "managed_id"
     t.index ["user_id"], name: "index_stripe_accounts_on_user_id"
   end
 
